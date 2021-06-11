3847d77ddb205d6076357dc9f5e4d035632525a5
Alejandro Silva <a.s.cebreros@gmail.com>
2021-06-11 13:17:13 -0500

    Implementaciones de es7 agregadas

---
+++ src/es7/index.js
@@ -0,0 +1,17 @@
+// Includes
+
+let numbers = [1,2,3,7,8];
+
+if(numbers.includes(7)) {
+    console.log('Si se encuentra el valor 7');
+} else (
+    console.log('No se encuentra')
+);
+
+// Elevar a la potencia
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result)
